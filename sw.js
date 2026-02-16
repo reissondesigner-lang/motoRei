8a744f2b20e26786ebaadf47759fef0886849bb3
reissondesigner-lang <reissondesigner@gmail.com>
2026-02-15 21:48:28 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,14 @@
-const CACHE_NAME = 'moto-panel-v3'; // Incremente a versão sempre que mudar o index
+const CACHE_NAME = 'motopanel-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  'https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&display=swap'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
-// Instalação: Salva os arquivos essenciais no cache
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
-  self.skipWaiting();
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-// Ativação: Limpa caches antigos de versões anteriores
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
-  );
-});
-
-// Busca (Fetch): Tenta carregar do cache primeiro para ser instantâneo
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
